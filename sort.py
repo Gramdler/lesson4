996992bf8114292c1220a81055430c1f0c64e0c9
Gramdler <itaferi@gmail.com>
2021-03-04 00:25:36 +0200

    working version
    This is work version  (like mpv)

--- sort.py
+++ sort.py
@@ -1,42 +1,43 @@
 ###This script sorts files in the folder which user give in an argument when open script in console. And print result in console###
 
 import sys
-import os
 from pathlib import Path
 
-# Lists for works:
-list_formatted = {
-    "image": [],
-    "video": [],
-    "doc": [],
-    "music": [],
-    "archive": [],
-    "unidentified": [],
-    "exists_formats": [],
-    "unexists_formats": []
-}  # vocabluary for result function
-
 # Start to work with functions
+#enclosure = False
 
 
-def print_vocabluary(vocabluary={}):
-    print("Congragulation!\nYour files had been sorted:")
-    for key, values in list_formatted.items():
-        if list_formatted[key] != []:
+def print_vocabluary(vocabluary={}, p=str):
+    print("_______________________________________")
+    print(
+        f"Congragulation!\nYour files in folder: {p}\n\thad been sorted:")
+    for key, values in vocabluary.items():
+        if vocabluary[key] != []:
             print(f"{key.title()} :", end="")
             for value in set(values):
                 print(f" {value}", end='')
             print()
     print()
-    list_formatted.clear()
     print("Thank you for take our service!")
 
 
 def sort(folder_path=Path()):
+
     # function sort files in formats in folder.
     # Folder_path is folder path in string, formats is vocabluary
     # where keys are the name of kind format and values are files extension
     # Give result in vocaluary like formats variant with addition keys know_extension and unknow_extension###
+    list_formatted = {
+        "image": [],
+        "video": [],
+        "doc": [],
+        "music": [],
+        "archive": [],
+        "unidentified": [],
+        "exists_formats": [],
+        "unexists_formats": []
+    }  # vocabluary for result function
+
     formats = {
         "image": ["JPEG", "PNG", "JPG", "SVG"],
         "video": ["AVI", "MP4", "MOV", "MKV"],
@@ -53,7 +54,6 @@ def sort(folder_path=Path()):
             if i.suffix:
                 for key, values in formats.items():
                     if i.suffix[1:].upper() in values:
-                        # if key in list_formatted.keys():
                         list_formatted[key].append(i.name)
                         list_formatted["exists_formats"].append(
                             f"{i.suffix[1:].upper()}")
@@ -71,25 +71,19 @@ def sort(folder_path=Path()):
         for value in suffix_list:
             if value not in list_formatted["exists_formats"]:
                 list_formatted["unexists_formats"].append(value)
+    print_vocabluary(list_formatted, p)
     for i in p.iterdir():
-        if i.is_dir():
-            # if [f for f in os.listdir(i.name) if not f.startswith('.')] == []:
-            #     break
-            # else:
-            if not i.name.startswith('.'):
-                print(f"files from enclosure folder {i.name}: ")
-                vocabluary_enclosure = sort(f"{i.name}")
-                print_vocabluary(vocabluary_enclosure)
-                vocabluary_enclosure.clear()
+        if i.is_dir() and not i.name.startswith('.'):
+            #print(f"files from enclosure folder {i.name}: ")
+            # enclosure
+            sort(f"{Path.cwd()}\{p.name}\{i.name}")
         elif i.is_file():
             continue
         else:
             print("If you see this message, that something went wrong")
-    return list_formatted
+
 
 # Take first argument from conslone after name of script, and check variant if user didn't give way.
-
-
 try:
     folder_path = sys.argv[1]
 except IndexError:
@@ -98,13 +92,8 @@ except IndexError:
 # Check condition if script take wrong way.
 
 if folder_path == "Error path":
-    print("You didn't enter in console folder path.\nIf you want to sort filest in folder where this script enter Y\n to finish program enter N or anything: ")
-    tmp = input("Y/N: ")
-    folder_path = Path() if tmp.lower() == "y" else exit()
+    print("You didn't enter in console folder path.\nIf you want to sort files in the current folder, enter Y\n to finish program enter N or anything: ")
+    tmpb = input("Y/N: ")
+    folder_path = Path() if tmpb.lower() == "y" else exit()
 
-#
-# sort(p, formats) if p.exist1 else print("You give wrong way, try again with Right way!")
-tmp = sort(folder_path)
-print_vocabluary(tmp)
-
-# Result print area:
+sort(folder_path)
